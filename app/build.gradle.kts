3b605ddf83f81e749aaf700e0c962196c5ab795b
Hank <eqpc@hbn.dev>
2026-06-08 22:41:11 +0200

    adding build action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,31 +11,47 @@ android {
     namespace = "no1.share.to.clipboard"
     compileSdk = 37
 
-    signingConfigs {
-        create("DefaultSigningKey") {
-            if (
-                !project.hasProperty("STC_KEY_STORE_FILE_PATH") ||
-                !project.hasProperty("STC_KEY_STORE_FILE_PASS") ||
-                !project.hasProperty("STC_KEY_STORE_ALIAS_NAME") ||
-                !project.hasProperty("STC_KEY_STORE_ALIAS_PASS")
-            ) {
-                throw GradleException(
-                    """
+    val keystorePath = findProperty("STC_KEY_STORE_FILE_PATH") as String?
+    if (keystorePath != null) {
+        signingConfigs {
+            create("DefaultSigningKey") {
+                var exceptionMessage = """
                             Please define signing properties in ~/.gradle/gradle.properties like below:
                             STC_KEY_STORE_FILE_PATH=/path/to/key/store/file
                             STC_KEY_STORE_FILE_PASS=key-store-password
                             STC_KEY_STORE_ALIAS_NAME=key-alias-name
                             STC_KEY_STORE_ALIAS_PASS=key-alias-password
                             """.trimIndent()
-                )
+                if (!project.hasProperty("STC_KEY_STORE_FILE_PATH")) {
+                    throw GradleException(
+                        exceptionMessage.replace("STC_KEY_STORE_FILE_PATH", "this line -> STC_KEY_STORE_FILE_PATH")
+                    )
+                }
+                if (!project.hasProperty("STC_KEY_STORE_FILE_PASS")) {
+                    throw GradleException(
+                        exceptionMessage.replace("STC_KEY_STORE_FILE_PASS", "this line -> STC_KEY_STORE_FILE_PASS")
+                    )
+                }
+                if (!project.hasProperty("STC_KEY_STORE_ALIAS_NAME")) {
+                    throw GradleException(
+                        exceptionMessage.replace("STC_KEY_STORE_ALIAS_NAME", "this line -> STC_KEY_STORE_ALIAS_NAME")
+                    )
+                }
+                if (!project.hasProperty("STC_KEY_STORE_ALIAS_PASS")) {
+                    throw GradleException(
+                        exceptionMessage.replace("STC_KEY_STORE_ALIAS_PASS", "this line -> STC_KEY_STORE_ALIAS_PASS")
+                    )
+                }
+
+                storeFile = file(project.property("STC_KEY_STORE_FILE_PATH") as String)
+                storePassword = project.property("STC_KEY_STORE_FILE_PASS") as String
+                keyAlias = project.property("STC_KEY_STORE_ALIAS_NAME") as String
+                keyPassword = project.property("STC_KEY_STORE_ALIAS_PASS") as String
             }
 
-            storeFile = file(project.property("STC_KEY_STORE_FILE_PATH") as String)
-            storePassword = project.property("STC_KEY_STORE_FILE_PASS") as String
-            keyAlias = project.property("STC_KEY_STORE_ALIAS_NAME") as String
-            keyPassword = project.property("STC_KEY_STORE_ALIAS_PASS") as String
         }
-
+    } else {
+        print("STC_KEY_STORE_FILE_PATH is not defined.")
     }
 
     defaultConfig {
